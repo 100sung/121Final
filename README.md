0020d5eb4f47f69107d69e6ccadcdce4ac013a6e
Sung_Baek <shbaek@ucsc.edu>
2019-03-05 11:57:17 -0800

    asdf

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 121Final
 final project for cmpm 121
+
+Assets:
+Down Town Pack-Lite: https://assetstore.unity.com/packages/3d/props/down-town-pack-lite-77864
---
+++ game/Assets/scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float run;
+    public float jump;
+    public Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // movement
+        body.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, 
+            body.velocity.y, Input.GetAxis("Vertical") * speed);
+        // run
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            body.velocity = new Vector3(Input.GetAxis("Horizontal") * run,
+            body.velocity.y, Input.GetAxis("Vertical") * run);
+        }
+        // jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            body.velocity = new Vector3(body.velocity.x, jump, body.velocity.z);
+        }
+    }
+}
